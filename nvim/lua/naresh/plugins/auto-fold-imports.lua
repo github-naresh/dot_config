d655935309a0b09462fc748540c1bf1f244d01f1
Naresh Maharjan <nareshmaharzan@gmail.com>
2023-09-22 17:00:34 +1000

    remove toggleterm and update auto-fold-import plugin

--- nvim/lua/naresh/plugins/auto-fold-imports.lua
+++ nvim/lua/naresh/plugins/auto-fold-imports.lua
@@ -1,15 +1,5 @@
 return {
   "github-naresh/auto-fold-imports.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local auto_fold = require("auto-fold-imports")
-    local augroup = vim.api.nvim_create_augroup("AutoFoldImports", {})
-    vim.api.nvim_clear_autocmds({ group = augroup })
-    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-      group = augroup,
-      callback = function()
-        auto_fold.setup()
-      end,
-    })
-  end,
+  config = true,
 }
